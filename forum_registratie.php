cf4f65f537324e6f29778959e10787b9e8007f44
Maris-Evers <159010642+Maris-Evers@users.noreply.github.com>
2024-06-03 14:18:57 +0200

    Add files via upload

---
+++ forum_registratie.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registratie</title>
+</head>
+
+<body>
+    <H1>Forum registratie</H1>
+    <form method="post" action="registratie_handler.php">
+        <input type="text" name="name"> Name
+        <br><br>
+        <input type="email" name="email"> Email
+        <br><br>
+        <input type="number" name="age"> Age
+        <br><br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
---
+++ registratie_handler.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <H1>Forum registratie</H1>
+    Jouw username is <?php echo $_POST["name"]; ?><br><br>
+    Jouw email is <?php echo $_POST["email"]; ?><br><br>
+    Jouw age is <?php echo $_POST["age"]; ?><br>
+</body>
+
+</html>
